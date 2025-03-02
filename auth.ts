73a70fd63f0ebe96ea29d5c17d278ebec5a97b3b
TakeruNakashima00 <takeru.nakashima00@gmail.com>
2025-03-02 21:14:12 +0900

    add auth.ts and middleware.ts

---
+++ auth.ts
@@ -0,0 +1,24 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const config: NextAuthConfig = {
+  providers: [GitHub],
+  basePath: "/api/auth",
+  callbacks: {
+    authorized({ request, auth }) {
+      try {
+        const { pathname } = request.nextUrl;
+        if (pathname === "/protected-page") return !!auth;
+        return true;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    jwt({ token, trigger, session }) {
+      if (trigger === "update") token.name = session.user.name;
+      return token;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
